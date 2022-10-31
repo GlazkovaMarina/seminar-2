e6a98d433ec83f0c126b2358a6a5f526ac0881f2
Daniil Pilipenko <sortedmap@gmail.com>
2022-10-31 22:59:14 +0300

    add count of words

--- main.js
+++ main.js
@@ -20,6 +20,18 @@
         }
     }
 
+    let countWords = function(text) {
+        let wordRegex = /[a-zA-Zа-яА-Я0-9]/;
+        let count = 0;
+        let tokens = text.split(/\s+/);
+        for(i in tokens) {
+            if(tokens[i].match(wordRegex)) {
+                count++;
+            }
+        }
+        return count;
+    };
+
     document.addEventListener('DOMContentLoaded', function(){
         let textArea = document.querySelector('#text');
         let result = document.querySelector('#result');
@@ -29,9 +41,10 @@
         countButton.addEventListener('click', function(){
             let text = textArea.value;
             let withoutSpaces = text.length - countSpaces(text);
+            let wordsCount = countWords(text);
             result.innerHTML = '<b>Длина текста:</b> ' +
                 text.length + ' символов<br>' +
-                '<b>Без пробелов:</b> ' + withoutSpaces + '<br>';
+                '<b>Без пробелов:</b> ' + withoutSpaces +  '<br>' + '<b>Количество слов:</b> ' + wordsCount;
         });
 
         textArea.addEventListener('keyup', function(){
